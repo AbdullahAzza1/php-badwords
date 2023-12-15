292f4996facb8253dcb9c50a0e36c9c988c90f68
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-12-15 16:44:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php $nome = "prova php kurwa" ?> 
+</head>
+<body>
+    <h1><?php echo "prima volta su php" ?></h1>
+    <div><?php echo $nome ?></div>
+</body>
+</html>
